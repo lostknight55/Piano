918e34e61ef0014ad6e9b019ce64151e37285d30
lostknight55 <76058832+lostknight55@users.noreply.github.com>
2021-03-05 05:22:06 -0500

    first commit

---
+++ scrip.js
@@ -0,0 +1,152 @@
+
+
+
+
+
+function playNoteC() {
+    var soundC = document.getElementById("c_note");
+    soundC.currentTime = 0;
+    soundC.play();
+  }
+
+  function playNoteCsharp() {
+    var sound = document.getElementById("c_sharp_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  
+  function playNoteD() {
+    var sound = document.getElementById("d_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteDsharp() {
+    var sound = document.getElementById("d_sharp_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteE() {
+    var sound = document.getElementById("e_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+   function playNoteF() {
+    var sound = document.getElementById("f_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteFsharp() {
+    var sound = document.getElementById("f_sharp_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteG() {
+    var sound = document.getElementById("g_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteGsharp() {
+    var sound = document.getElementById("g_sharp_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteA() {
+    var sound = document.getElementById("a_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteAsharp() {
+    var sound = document.getElementById("a_sharp_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteB() {
+    var sound = document.getElementById("b_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+   function playNoteC2() {
+    var sound = document.getElementById("c2_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteC2sharp() {
+    var sound = document.getElementById("c2_sharp_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteD2() {
+    var sound = document.getElementById("d2_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteD2sharp() {
+    var sound = document.getElementById("d2_sharp_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteE2() {
+    var sound = document.getElementById("e2_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteF2() {
+    var sound = document.getElementById("f2_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteF2sharp() {
+    var sound = document.getElementById("f2_sharp_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteG2() {
+    var sound = document.getElementById("g2_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteG2sharp() {
+    var sound = document.getElementById("g2_sharp_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteA2() {
+    var sound = document.getElementById("a2_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteA2sharp() {
+    var sound = document.getElementById("a2_sharp_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+  function playNoteB2() {
+    var sound = document.getElementById("b2_note");
+    sound.currentTime = 0;
+    sound.play();
+  }
+
+
+  
